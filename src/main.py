c49bae676967b92a1007a4e13e4b57866c0ef811
mohammad2928 <zaribar2928@gmail.com>
2021-07-16 17:37:45 +0430

    multiproces version added

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
-import cv2
 from ocr import OCR
 from image_preprocessing import img_preprocessing
 from analyse import get_personal_info, get_tumor_info
+import multiprocessing
+
+
+
+def do_ocr(ocr_type, pre_processed_image, return_dict):
+    ocr = OCR()
+    ocr_text = ocr_type(pre_processed_image)
+    name = str(ocr_type).split('.')[1]
+    name = name.split(' ')[0]
+    return_dict[name] = ocr_text
+
+
+def multi_process(ocr_types, pre_processed_image, return_dict):
+    jobs = []
+    for i in ocr_types.values():
+        p = multiprocessing.Process(target=do_ocr, args=(i, pre_processed_image, return_dict))
+        jobs.append(p)
+        p.start()
+
+    for proc in jobs:
+        proc.join()
 
 
 def main():
@@ -9,13 +29,20 @@ def main():
     ocr = OCR()
     pre_processed_image = img_preprocessing(
         sample_image_path
-    )  # preprocessing input image
+        )  # preprocessing input image
 
-    english_result = ocr.english_tesseract_ocr(pre_processed_image)  # English OCR
-    persian_result = ocr.persian_tesseract_ocr(pre_processed_image)  # Persian OCR
+    ocr_types = {
+    'persain_easyocr': ocr.persian_easyocr,
+    'english_easyocr': ocr.english_easyocr,
+    'persian_tesseract_ocr': ocr.persian_tesseract_ocr,
+    'english_tesseract_ocr': ocr.english_tesseract_ocr,  
+        }
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    multi_process(ocr_types, pre_processed_image, return_dict)
 
-    get_personal_info(persian_result.split("\n"))
-    get_tumor_info(persian_result.split("\n"))
+    get_personal_info(return_dict['persian_tesseract_ocr'].split("\n"))
+    get_tumor_info(return_dict['persian_tesseract_ocr'].split("\n"))
 
 
 if __name__ == "__main__":
